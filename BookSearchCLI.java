9bbbfb6b58608ff10a7527b88bb02b5f66dc5581
andersonal25 <103077426+andersonal25@users.noreply.github.com>
2022-04-10 16:51:16 -0400

    Add files via upload

---
+++ BookSearchCLI.java
@@ -0,0 +1,22 @@
+public class BookSearchCLI {
+
+    public String searchByTitle(String title){
+
+    }
+
+    public String searchByAuthor(String author){
+
+    }
+
+    public String search(String title, String author){
+
+    }
+
+    private displayMenu(){
+
+    }
+
+    private displayResults(){
+        
+    }
+}
