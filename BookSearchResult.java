a85d95cf24e675346c3c8edff92a6a66140d6f1b
andersonal25 <103077426+andersonal25@users.noreply.github.com>
2022-05-02 17:03:56 -0400

    Add files via upload

--- BookSearchResult.java
+++ BookSearchResult.java
@@ -1,3 +1,15 @@
-public class BookSearchResult {
-    
+public class BookSearchResult extends Result{
+    Book book;
+
+    public List<Book> searchByTitle(String title){
+
+    }
+
+    public List<Book> searchByAuthor(String author){
+
+    }
+
+    public List<Book> searchByISBN(String ISBN){
+
+    }
 }
