3316d5bf7cbf7b54c7b3ac65da647bc782cfec7b
andersonal25 <103077426+andersonal25@users.noreply.github.com>
2022-05-04 22:29:19 -0400

    Add files via upload

--- SearchBy.java
+++ SearchBy.java
@@ -1,3 +1,7 @@
+/**
+ * SearchBy enum
+ * defines book fields that can be searched via keywords
+ */
 public enum SearchBy {
-    Any, Title, Author, Subject, ISBN;
+    Any, Title, Author, Subject, ISBN, NONE;
 }
